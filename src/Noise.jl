728e8046b78c8e495838c1327f69ba91ade52a27
Stefano Scali <scali.stefano@gmail.com>
2022-06-27 15:06:26 +0100

    few comments and layout changed in Noise.jl

--- src/Noise.jl
+++ src/Noise.jl
@@ -1,18 +1,21 @@
 ## Noise type ##
 abstract type Noise end
 
+## Classical noise ##
 struct ClassicalNoise{TT<:Real} <: Noise
   T::TT
 end
 
 spectrum(n::ClassicalNoise) = ω -> iszero(ω) ? zero(ω) : 2*n.T/ω
 
+## Quantum noise ##
 struct QuantumNoise{TT<:Real} <: Noise
   T::TT
 end
 
 spectrum(n::QuantumNoise) = ω -> iszero(n.T) ? sign(ω) : (iszero(ω) ? zero(ω) : coth(ω/n.T/2))
 
+## Barker noise ##
 struct BarkerNoise{TT<:Real} <: Noise
   T::TT
 end
