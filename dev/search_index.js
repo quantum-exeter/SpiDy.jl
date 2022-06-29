015269d8db59f410b7fbf553c2bd8889ed75e7b2
Documenter.jl <documenter@juliadocs.github.io>
2022-06-29 10:43:21 +0000

    build based on 3d0163f

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Power-spectral-density","page":"Power spectral density","title":"Power spectral density","text":"","category":"section"},{"location":"","page":"Power spectral density","title":"Power spectral density","text":"PSD(J::SpectralDensity, noise::Noise)","category":"page"}]
+[{"location":"#Power-spectral-density","page":"Power spectral density","title":"Power spectral density","text":"","category":"section"},{"location":"","page":"Power spectral density","title":"Power spectral density","text":"PSD(J::SpectralDensity, noise::Noise)\nb_field(N, Δt, J::SpectralDensity, noise::Noise, distro=nothing)","category":"page"}]
 }
