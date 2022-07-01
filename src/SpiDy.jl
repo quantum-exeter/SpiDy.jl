cef17b3a341e2b933db50007b1a6b03f0ca976ff
Stefano Scali <scali.stefano@gmail.com>
2022-07-01 18:40:17 +0100

    Change name of CouplingFunction

--- src/SpiDy.jl
+++ src/SpiDy.jl
@@ -10,11 +10,11 @@ include("Noise.jl");
 include("SpectralDensity.jl");
 include("StochasticField.jl");
 include("SpinState.jl");
-include("CouplingFunction.jl");
+include("CouplingTensor.jl");
 include("Dynamics.jl");
 
 # export external files to build documentation
-export Noise, SpectralDensity, StochasticField, SpinState, CouplingFunction, Dynamics
+export Noise, SpectralDensity, StochasticField, SpinState, CouplingTensor, Dynamics
 # export structures to build documentation
 export ClassicalNoise, QuantumNoise, NoZeroQuantumNoise, GenericSD, LorentzianSD
 # export modules and functions to build documentation
