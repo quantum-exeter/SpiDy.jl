c2d9196233deab4993b82b13c2c6024a356d1453
Stefano Scali <scali.stefano@gmail.com>
2022-07-04 19:53:54 +0100

    Add interpolation in bfield

--- src/StochasticField.jl
+++ src/StochasticField.jl
@@ -7,10 +7,11 @@ The sampling of the stochastic noise is done in frequency space. The default sto
 is white noise having Gaussian distribution but different distributions can be specified.
 N defines the number of steps and Δt defines the time step.
 """
-function bfield(N, Δt, J::GenericSD, noise::Noise, distro=Normal(0., 1/sqrt(Δt)))
+function bfield(N, Δt, J::GenericSD, noise::Noise; distro=Normal(0., 1/sqrt(Δt)), interpolation=true)
     distrosample = rand(distro, N)
     distrosamplefft = rfft(distrosample)
     psdfft = 2π*psd(J, noise).(2π*rfftfreq(N, 1/Δt)) # NB: rfftfreq(N, 1/Δt) takes the frequency step in input!
     bfft = sqrt.(psdfft).*distrosamplefft
-    return irfft(bfft, N)
+    b = irfft(bfft, N)
+    return interpolation ? LinearInterpolation(LinRange(0, N*Δt, N), b) : b
 end
