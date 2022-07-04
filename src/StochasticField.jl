5177f40f4db570d07e557ad2f45fce31653ed94b
Stefano Scali <scali.stefano@gmail.com>
2022-07-04 09:21:09 +0100

    Resolve issue #2 on default distribution in bfield

--- src/StochasticField.jl
+++ src/StochasticField.jl
@@ -7,10 +7,7 @@ The sampling of the stochastic noise is done in frequency space. The default sto
 is white noise having Gaussian distribution but different distributions can be specified.
 N defines the number of steps and Δt defines the time step.
 """
-function bfield(N, Δt, J::GenericSD, noise::Noise, distro=nothing)
-    if isnothing(distro)
-        distro = Normal(0., 1/sqrt(Δt))
-    end
+function bfield(N, Δt, J::GenericSD, noise::Noise, distro=Normal(0., 1/sqrt(Δt)))
     distrosample = rand(distro, N)
     distrosamplefft = rfft(distrosample)
     psdfft = 2π*psd(J, noise).(2π*rfftfreq(N, 1/Δt)) # NB: rfftfreq(N, 1/Δt) takes the frequency step in input!
