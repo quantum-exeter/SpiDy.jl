eea731f2505c2d6fb29e0f77232d29ed270a1d78
Stefano Scali <scali.stefano@gmail.com>
2022-07-04 14:23:11 +0100

    Update diffeqsolver to define Normal distribution as default distro

--- src/Dynamics.jl
+++ src/Dynamics.jl
@@ -1,4 +1,4 @@
-function diffeqsolver(N, Δt, J::GenericSD, noise::Noise, distro=nothing)
+function diffeqsolver(N, Δt, J::GenericSD, noise::Noise, Cω::Coupling, distro=Normal(0., 1/sqrt(Δt)))
 
 end
 
