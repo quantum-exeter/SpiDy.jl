a65973165538e6f8a07ed93574a282ebafec6b50
Stefano Scali <scali.stefano@gmail.com>
2022-07-05 09:54:40 +0100

    Tuning of Dynamics style and movert

--- src/Dynamics.jl
+++ src/Dynamics.jl
@@ -1,9 +1,11 @@
 function diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; S0=1/2, Bext=[0, 0, 1])
+
     u0 = [s0[1], s0[2], s0[3], 0, 0, 0, 0, 0, 0]
     Cω2 = matrix.C*transpose(matrix.C)
     bn = t -> matrix.C*[bfields[1](t), bfields[2](t), bfields[3](t)];
+    
     function f(du, u, p, t)
-        s = @view u[1:3] # not allocating values. no hard copy.
+        s = @view u[1:3] # @view does not allocate values. No hard copy, just reference.
         v = @view u[4:6]
         w = @view u[7:9]
         du[1:3] = cross(s, Bext + bn(t)/S0 + Cω2*v)
@@ -12,10 +14,12 @@ function diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; S0=
     end
     prob = ODEProblem(f, u0, tspan)
     sol = solve(prob, Vern7(), abstol=1e-8, reltol=1e-8, maxiters=Int(1e7))
+    
     s = zeros(length(sol.t), 3)
     for n in 1:length(sol.t)
-        s[n,:] = sol.u[n][1:3]
+        s[n, :] = sol.u[n][1:3]
     end
     sinterp = t -> sol(t)[1:3]
-    return sinterp, sol.t, s
+    
+    return sol.t, s, sinterp
 end
--- src/movert.jl
+++ src/movert.jl
@@ -2,37 +2,38 @@ include("./SpiDy.jl")
 using .SpiDy
 
 using Statistics
-
 using ProgressBars
 using NPZ
+using Random
+using LinearAlgebra
 
 Δt = 0.15
 N = 72_000
 tspan = (0., N*Δt)
 
-J = LorentzianSD(1., 7., 5.);
+# Lorentzian(α, ω0, Γ)
+J = LorentzianSD(1., 7., 5.); # prm 5
 
 matrix = AnisoCoupling([-sin(π/4) 0. 0.
-          0. 0. 0.
-          cos(π/4) 0. 0.]);
+                        0. 0. 0.
+                        cos(π/4) 0. 0.]);
 
-T = 10 .^ LinRange(-3, 2, 12)
+T = 10 .^ LinRange(-2, 2, 12)
 Sss = zeros(length(T), 3)
 
-navg = 12
-
+navg = 5
 Threads.@threads for n in ProgressBar(1:length(T))
     noise = ClassicalNoise(T[n]);
     s = zeros(navg, 3)
-    for k in 1:navg
+    for i in 1:navg
+        s0 = normalize(rand(3)) # [0.8, 0., -0.6]
         bfields = [bfield(N, Δt, J, noise),
-                bfield(N, Δt, J, noise),
-                bfield(N, Δt, J, noise)];
-        sol = diffeqsolver([0.8, 0., -0.6], tspan, J, bfields, matrix);
-        # s[k,:] = mean(sol[3][end-(length(sol[2])÷4):end,:], dims=1)
-        s[k,:] = mean([sol[1](t)[n] for t in (3*N÷4)*Δt:Δt:N*Δt, n in 1:3], dims=1)
+                   bfield(N, Δt, J, noise),
+                   bfield(N, Δt, J, noise)];
+        sol = diffeqsolver(s0, tspan, J, bfields, matrix);
+        s[i, :] = mean([sol[3](t)[n] for t in (N*3÷4)*Δt:Δt:N*Δt, n in 1:3], dims=1)
     end
-    Sss[n,:] = mean(s, dims=1)
+    Sss[n, :] = mean(s, dims=1)
 end
 
-npzwrite("run.npz", Dict("T" => T, "S" => Sss))
+npzwrite("../notebooks/data.npz", Dict("T" => T, "S" => Sss))
