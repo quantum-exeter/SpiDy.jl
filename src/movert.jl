d4635f635e244ca4cb1c5a6b2d3bd62ddfa90779
Stefano Scali <scali.stefano@gmail.com>
2022-07-07 13:28:04 +0100

    Change movert.jl to use saveat option, progressmeter and changed threading

--- src/movert.jl
+++ src/movert.jl
@@ -2,14 +2,15 @@ include("./SpiDy.jl")
 using .SpiDy
 
 using Statistics
-using ProgressBars
+using ProgressMeter
 using NPZ
 using Random
 using LinearAlgebra
 
-Δt = 0.05
-N = 100_000
+Δt = 0.15
+N = 72_000
 tspan = (0., N*Δt)
+saveat = ((N*4÷5):1:N)*Δt
 
 # Lorentzian(α, ω0, Γ)
 J = LorentzianSD(1., 7., 5.); # prm 5
@@ -21,19 +22,21 @@ matrix = AnisoCoupling([-sin(π/4) 0. 0.
 T = 10 .^ LinRange(-3, 3, 12)
 Sss = zeros(length(T), 3)
 
-navg = 8
-Threads.@threads for n in ProgressBar(1:length(T))
+navg = 24
+p = Progress(length(T));
+for n in 1:length(T)
     noise = ClassicalNoise(T[n]);
     s = zeros(navg, 3)
-    for i in 1:navg
-        s0 = normalize(rand(3)) # [0.8, 0., -0.6]
+    Threads.@threads for i in 1:navg
+        s0 = [0.8, 0., -0.6] #normalize(rand(3))
         bfields = [bfield(N, Δt, J, noise),
                    bfield(N, Δt, J, noise),
                    bfield(N, Δt, J, noise)];
-        sol = diffeqsolver(s0, tspan, J, bfields, matrix, Bext=[0, 0, 10]);
-        s[i, :] = mean([sol[3](t)[n] for t in (N*4÷5)*Δt:Δt:N*Δt, n in 1:3], dims=1)
+        sol = diffeqsolver(s0, tspan, J, bfields, matrix; saveat=saveat);
+        s[i, :] = mean(sol[2], dims=1)
     end
     Sss[n, :] = mean(s, dims=1)
+    next!(p)
 end
 
-npzwrite("../notebooks/data.npz", Dict("T" => T, "S" => Sss))
+npzwrite("../notebooks/data_original.npz", Dict("T" => T, "S" => Sss))
