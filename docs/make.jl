34c08e98dabbf7a0f91858659214e06dfb1e7b48
Stefano Scali <scali.stefano@gmail.com>
2022-07-21 15:09:13 +0100

    Fix make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,12 @@ push!(LOAD_PATH,"../src/")
 using Documenter, SpiDy
 
 makedocs(sitename="SpiDy.jl",
-    pages = ["Index" => "src/index.md",
-             "Noise" => "src/noise.md",
-             "Spectral density" => "src/spectraldensity.md",
-             "Stochastic field" => "src/stochasticfield.md",
-             "Coupling tensor" => "src/couplingtensor.md",
-             "Dynamics" => "src/dynamics.md"])
+    pages = ["Index" => "index.md",
+             "Noise" => "noise.md",
+             "Spectral density" => "spectraldensity.md",
+             "Stochastic field" => "stochasticfield.md",
+             "Coupling tensor" => "couplingtensor.md",
+             "Dynamics" => "dynamics.md"])
 
 deploydocs(
     repo = "github.com/quantum-exeter/SpiDy.jl.git",
