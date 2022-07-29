c425fd27ed1825dd4afd4e734db5358390647e88
Stefano Scali <scali.stefano@gmail.com>
2022-07-29 11:17:20 +0100

    Harmonic oscillator implementation as system H - by Charlie

---
+++ runs/run_dynamics _ho.jl
@@ -0,0 +1,87 @@
+using SpiDy
+using NPZ
+using DataFrames
+using CSV
+using ProgressMeter
+using Random
+using Statistics
+using LinearAlgebra
+using Plots
+
+########################
+########################
+
+Δt = 0.01
+N = 50_000
+tspan = (0., N*Δt)
+saveat = (0:1:N)*Δt
+
+J = LorentzianSD(10., 7., 5.); # (α, ω0, Γ)
+
+matrix = IsoCoupling([1.]);
+
+noise = ClassicalNoise(10.);
+
+x0 = [1., 0., 0.]
+p0 = [0., 0., 0.]
+
+navg = 20
+
+########################
+########################
+
+println("Starting...")
+
+p = Progress(navg);
+solx = zeros(navg, length(saveat), 3)
+solp = zeros(navg, length(saveat), 3)
+
+Threads.@threads for i in 1:navg
+    bfields = [bfield(N, Δt, J, noise),
+               bfield(N, Δt, J, noise),
+               bfield(N, Δt, J, noise)];
+    sol = diffeqsolver(x0, p0, tspan, J, bfields, matrix; saveat=saveat);
+    solx[i, :, :] = sol[2]
+    solp[i, :, :] = sol[3]
+    next!(p)
+end
+
+solavgx = mean(solx, dims=1)[1, :, :];
+solavgp = mean(solp, dims=1)[1, :, :];
+
+########################
+########################
+
+### Save data NPZ ###
+# npzwrite("./dynamics.npz", Dict("t" => saveat,
+#                                 "x" => solavgx,
+#                                 "p" => solavgp))
+
+### Save data CSV ###
+# dataframe = DataFrame(t = saveat,
+#                       xx = solavgx[:, 1],
+#                       xy = solavgx[:, 2],
+#                       xz = solavgx[:, 3],
+#                       px = solavgp[:, 1],
+#                       py = solavgp[:, 2],
+#                       pz = solavgp[:, 3])
+# CSV.write("./dynamics.csv", dataframe)
+
+### Plots ###
+plot(saveat, solavg_x[:, 1], xlabel="t", ylabel="x_x")
+# savefig("./xx.pdf")
+
+# plot(saveat, solavg[:, 2], xlabel="t", ylabel="x_y")
+# savefig("./xy.pdf")
+
+# plot(saveat, solavg[:, 3], xlabel="t", ylabel="x_z")
+# savefig("./xz.pdf")
+
+# plot(saveat, solavg_x[:, 1], xlabel="t", ylabel="p_x")
+# savefig("./px.pdf")
+
+# plot(saveat, solavg[:, 2], xlabel="t", ylabel="p_y")
+# savefig("./py.pdf")
+
+# plot(saveat, solavg[:, 3], xlabel="t", ylabel="p_z")
+# savefig("./pz.pdf")
--- src/Dynamics.jl
+++ src/Dynamics.jl
@@ -5,7 +5,7 @@ diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; S0=1/2, Bext
 
 Returns `[sol.t, s, sinterp]`, that is, the vector `sol.t` of time steps at which the solutions are evaluated,
 the 3-vector of the solutions `s[1]`, `s[2]`, `s[3]` evaluated at times `sol.t`,
-the 3 functions sinterp(t)[i] interpolations of the solutions `s[i]` found in the give time span.
+the 3 functions sinterp(t)[i] interpolations of the solutions `s[i]` found in the given time span.
 
 The differential equation solver is built to account for Lorentzian spectral density.
 
@@ -45,4 +45,58 @@ function diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; S0=
     sinterp = t -> sol(t)[1:3]
     
     return sol.t, s, sinterp
+end
+
+"""
+```Julia
+diffeqsolver(x0, p0, tspan, J::LorentzianSD, bfields, matrix::Coupling; Ω=1.0, saveat=[])
+```
+
+Returns `[sol.t, x, p, xinterp, pinterp]`, that is, the vector `sol.t` of time steps at which the solutions are evaluated,
+the vectors of the solutions `x`, `p` evaluated at times `sol.t`,
+the functions xinterp(t), pinterp(t) interpolations of the solutions found in the given time span.
+
+The differential equation solver is built to account for Lorentzian spectral density.
+
+Keyword arguments:
+- `Ω` external magnetic field set as unit-vector along the z-axis as default, `Ω=1.0`
+- `saveat` is an option of the function `solve()` which allows to only save the solution at the points needed to evaluate the steady-state,
+i.e. at late times. Used to optimize memory management and speed of the solver. Default value is an empty list, `saveat=[]`, resulting
+in the solution saved at optimal time steps withing the entire time span.
+
+# Examples
+```julia-repl
+julia> diffeqsolver(x0, p0, tspan, J, bfields, matrix; saveat=((N*4÷5):1:N)*Δt)
+```
+"""
+function diffeqsolver(x0, p0, tspan, J::LorentzianSD, bfields, matrix::Coupling; Ω=1.0, saveat=[])
+
+    u0 = [x0[1], x0[2], x0[3], p0[1], p0[2], p0[3], 0, 0, 0, 0, 0, 0]
+    Cω2 = matrix.C*transpose(matrix.C)
+    bn = t -> matrix.C*[bfields[1](t), bfields[2](t), bfields[3](t)];
+    
+    function f(du, u, t)
+        x = @view u[1:3] # @view does not allocate values. No hard copy, just reference.
+        p = @view u[4:6]
+        v = @view u[7:9]
+        w = @view u[10:12]
+        du[1:3] = p
+        du[4:6] = -(Ω^2)*x + bn(t) + Cω2*v
+        du[7:9] = w
+        du[10:12] = -(J.ω0^2)*v -J.Γ*w + J.α*x
+    end
+    prob = ODEProblem(f, u0, tspan)
+    sol = solve(prob, Vern7(), abstol=1e-8, reltol=1e-8, maxiters=Int(1e7), saveat=saveat)
+
+    x = zeros(length(sol.t), 3)
+    p = zeros(length(sol.t), 3)
+    for n in 1:length(sol.t)
+        x[n, :] = sol.u[n][1:3]
+        p[n, :] = sol.u[n][4:6]
+    end
+    xinterp = t -> sol(t)[1:3]
+    pinterp = t -> sol(t)[4:6]
+    
+    return sol.t, x, p, xinterp, pinterp
+
 end
