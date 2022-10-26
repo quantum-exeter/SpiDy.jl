029c1c08082abf8e3b618e6c531bbb5bc91d5c0a
Stefano Scali <scali.stefano@gmail.com>
2022-10-26 15:44:59 +0100

    Cleaned if statements in dynamics and rework on documentation

--- src/Dynamics.jl
+++ src/Dynamics.jl
@@ -1,6 +1,6 @@
 """
 ```Julia
-diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; JH=zero(I), S0=1/2, Bext=[0, 0, 1], saveat=[])
+diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; JH=zero(I), S0=1/2, Bext=[0, 0, 1], saveat=[], project=true, alg=Tsit5(), atol=1e-3, rtol=1e-3)
 ```
 
 Returns `[sol.t, s, sinterp]`, that is,
@@ -15,7 +15,9 @@ Keyword arguments:
 - `S0` spin length set at default value 1/2, `S0=1/2`.
 - `Bext` external magnetic field set as unit-vector along the z-axis as default, `Bext = [0, 0, 1]`.
 - `saveat` is an option of the function `solve()` which allows to only save the solution at the points needed to evaluate the steady-state, i.e. at late times. Used to optimize memory management and speed of the solver. Default value is an empty list, `saveat=[]`, resulting in the solution being saved at optimal time steps within the time span.
-
+- `project` project the solution to force spin length conservation.
+- `alg` chooses the solving algorithm.
+- `atol` and `rtol` define the absolute and relative tolerance of the solver.
 # Examples
 ```julia-repl
 julia> diffeqsolver(s0, tspan, J, bfields, matrix; saveat=((N*4÷5):1:N)*Δt)
@@ -40,7 +42,6 @@ function diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; JH=
         du[4+3*N:6+3*N] = -(J.ω0^2)*v -J.Γ*w -J.α*sum([s[(1+(j-1)*3):(3+(j-1)*3)] for j in 1:N])
     end
     prob = ODEProblem(f, u0, tspan, (dualcache(zeros(3)), dualcache(zeros(3))))
-
     condition(u, t, integrator) = true
     function affect!(integrator)
         for n in 1:N
@@ -48,19 +49,14 @@ function diffeqsolver(s0, tspan, J::LorentzianSD, bfields, matrix::Coupling; JH=
         end
     end
     cb = DiscreteCallback(condition, affect!, save_positions=(false,true))
-    if project
-        skwargs = (callback=cb,)
-    else
-        skwargs = NamedTuple()
-    end
-
+    skwargs = project ? (callback=cb,) : NamedTuple()
     sol = solve(prob, alg, abstol=atol, reltol=rtol, maxiters=Int(1e7), save_idxs=1:3*N, saveat=saveat; skwargs...)
     return sol
 end
 
 """
 ```Julia
-diffeqsolver(x0, p0, tspan, J::LorentzianSD, bfields, matrix::Coupling; Ω=1.0, saveat=[])
+diffeqsolver(x0, p0, tspan, J::LorentzianSD, bfields, matrix::Coupling; JH=zero(I), Ω=1.0, saveat=[], alg=Tsit5(), atol=1e-3, rtol=1e-3)
 ```
 
 Returns `[sol.t, x, p, xinterp, pinterp]`, that is,
@@ -73,6 +69,9 @@ The differential equation solver is built to account for Lorentzian spectral den
 Keyword arguments:
 - `Ω` harmonic oscillator bare frequency set as default to `Ω=1.0`.
 - `saveat` is an option of the function `solve()` which allows to only save the solution at the points needed to evaluate the steady-state, i.e. at late times. Used to optimize memory management and speed of the solver. Default value is an empty list, `saveat=[]`, resulting in the solution being saved at optimal time steps within the time span.
+- `JH` is the Heisenberg coupling matrix. Note that this have to be a symmetric matrix with zero diagonal. The preset value is the additive identity of the UniformScaling type, `JH=zero(I)`.
+- `alg` chooses the solving algorithm.
+- `atol` and `rtol` define the absolute and relative tolerance of the solver.
 
 # Examples
 ```julia-repl
