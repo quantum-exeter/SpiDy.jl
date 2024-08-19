ed21dba45f20c84e0f212eedab18cc306f3a65f4
Federico Cerisola <federico@cerisola.net>
2024-08-19 20:27:06 +0100

    Commit fixed unit docs link

--- docs/make.jl
+++ docs/make.jl
@@ -14,6 +14,7 @@ makedocs(;
         assets=String[],
     ),
     pages = ["Start with SpiDy.jl" => "index.md",
+             "Units and parameter choice" => "units.md",
              "Noise" => "noise.md",
              "Spectral density" => "spectraldensity.md",
              "Stochastic field" => "stochasticfield.md",
